fb9e4e76a308256d97fb88b6c8535f16bac9341c
Valerie Loveland <valerieloveland@gmail.com>
2018-03-13 17:37:02 -0400

    Create 039.Printing.java

---
+++ Part1/Week2/039.Printing.java
@@ -0,0 +1,69 @@
+//import java.util.Scanner;
+
+public class Printing {
+  
+    public static void printStars(int amount) {
+        // 39.1
+        // you can print one star with the command
+        // System.out.print("*");
+        //System.out.println("*");
+        // call this command amount times
+        
+        for (int i=0; i<amount; i++){
+        System.out.print("*");
+            
+        }System.out.println("");
+              
+        
+              
+        
+    }
+       
+   
+
+   public static void printSquare(int sideSize) {
+        // 39.2
+        for(int i=0; i<sideSize; i++){
+            //System.out.println("");
+            //for (int j=0; j<sideSize;j++){
+                printStars(sideSize);
+                
+            //}
+        }
+        
+        
+          
+   }
+    public static void printRectangle(int width, int height) {
+       
+     for (int i=0; i< height; i++){
+         
+     }
+    }
+    public static void printTriangle(int size) {
+        
+        for(int i=1; i<=size;i++){
+            
+                printStars(i);
+            
+        }
+   
+    }
+
+    public static void main(String[] args) {
+        // Tests do not use main, you can write code here freely!
+        // if you have problems with tests, please try out first 
+        // here to see that the printout looks correct
+         
+       
+        printStars(3);
+       System.out.println("\n---");  // printing --- to separate the figures
+       printSquare(4);
+       System.out.println("\n---");
+       printRectangle(5, 6);
+        System.out.println("\n---");
+       printTriangle(3);
+       System.out.println("\n---");
+    }
+
+}
