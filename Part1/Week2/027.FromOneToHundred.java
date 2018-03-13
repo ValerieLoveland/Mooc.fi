5ead82e494ed479e787ceff515f156dfdb1aa006
Valerie Loveland <valerieloveland@gmail.com>
2018-03-13 17:20:20 -0400

    Create 027.FromOneToHundred.java

---
+++ Part1/Week2/027.FromOneToHundred.java
@@ -0,0 +1,11 @@
+
+public class FromOneToHundred {
+
+    public static void main(String[] args) {
+        int i=0;
+      for (i=1; i<101;  i++){
+          System.out.println(i);
+        
+    }
+    }
+}
