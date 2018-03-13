6ce394675d5cca03eaa946b532dd70fc93a93994
Valerie Loveland <valerieloveland@gmail.com>
2018-03-13 17:05:06 -0400

    Update 013.NhlStatisticsPart1.java

--- Part1/Week1/013.NhlStatisticsPart1.java
+++ Part1/Week1/013.NhlStatisticsPart1.java
@@ -1,19 +1,34 @@
 
-import java.util.Scanner;
 
-public class AgeOfMajority {
+import nhlstats.NHLStatistics;
+
+public class Main {
 
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
+ 
+//  Print the top ten players based on goals
+System.out.println("Top ten players based on goals: ");
+NHLStatistics.sortByGoals();
+NHLStatistics.top(10);
 
-        System.out.println("How old are you?");
-        int age = Integer.parseInt(reader.nextLine());
-        
-        if (age<18){
-        System.out.println("You have not reached the age of majority yet!");
-    }
-        if (age>=18){
-        System.out.println("You have reached the age of majority!");
-    }
+//Print the top 25 players based on penalty amounts
+System.out.println("Top 25 players based on penalty amounts: ");
+NHLStatistics.sortByPenalties();
+NHLStatistics.top(25);
+
+//Print the statistics for Sidney Crosby
+System.out.println("Statistics for Sidney Crosby:");
+NHLStatistics.searchByPlayer("Sidney Crosby");
+
+//Print the statistics for Philadelphia Flyers (abbreviation: PHI). Note in which order the players are printed in and why that might be!
+System.out.println("Statistics for Philadelphia Flyers: ");
+NHLStatistics.teamStatistics("PHI");
+NHLStatistics.sortByPoints();
+
+//Print the players in Anaheim Ducks (abbreviation: ANA) ordered by points
+System.out.println("Players in the Ducks ordered by points: ");
+NHLStatistics.teamStatistics("ANA");
+NHLStatistics.sortByPoints();
+NHLStatistics.top(100);
     }
 }
