3b218b5c7fdb3b8e617a4535d0bed2164c364373
Valerie Loveland <valerieloveland@gmail.com>
2018-03-13 17:18:58 -0400

    Create 029.EvenNumbers.java

---
+++ Part1/Week2/029.EvenNumbers.java
@@ -0,0 +1,11 @@
+
+public class EvenNumbers {
+
+    public static void main(String[] args) {
+        int i = 0;
+        for (i=1; i<101; i++)
+        if(i%2==0){
+            System.out.println(i);
+        }
+    }
+}
