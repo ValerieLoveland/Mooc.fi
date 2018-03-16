5b74d687cc7717fc17e73dceab3acefd99726b7f
Valerie Loveland <valerieloveland@gmail.com>
2018-03-15 22:02:58 -0400

    Create 050.FirstCharacters.java

---
+++ Part1/Week3/050.FirstCharacters.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FirstCharacters {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Type your name: ");
+        String name = reader.nextLine();
+        int measure = name.length();
+        if(measure >= 3){
+          
+        System.out.println("1. character: " + name.charAt(0));
+        System.out.println("2. character: " + name.charAt(1));
+        System.out.println("3. character: " + name.charAt(2));
+    }
+       
+    }
+    
+}
